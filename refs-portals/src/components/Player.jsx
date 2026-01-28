c59d85964e7abc18f336a7cd234700408935f407
cmurillo08 <ing.cmurillo@gmail.com>
2026-01-27 18:02:08 -0600

    Ref&Portals completed

--- refs-portals/src/components/Player.jsx
+++ refs-portals/src/components/Player.jsx
@@ -1,10 +1,22 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  // Connect ref to JSX elemennts using ref property 
+  const playerName = useRef();
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+
+  function handleClick() {
+    // current is the JSX element associated with the ref, in this case the <input>
+    setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = '';
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={playerName} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
